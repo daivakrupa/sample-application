f1ac624fb663a16715c4bff62ea5b9e6416f2733
Daivakrupa Thota <80198240+daivakrupa@users.noreply.github.com>
2026-03-24 16:22:09 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const getMessage = async () => {
-  const response = await fetch(`${API_URL}/api/message`);
+  const response = await fetch(`${API_URL}/health`);
   return response.json();
 };
