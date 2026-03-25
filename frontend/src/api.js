acc1ce76bab0e6b3de40ea03b2603a36fb6bdb93
Daivakrupa Thota <80198240+daivakrupa@users.noreply.github.com>
2026-03-25 11:48:44 +0530

    Update fetch URL in getMessage function

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const getMessage = async () => {
-  const response = await fetch(`${API_URL}/health`);
+  const response = await fetch(`${API_URL}/health/message`);
   return response.json();
 };
